d33259336847dbd976fcbb61d51818ee299cb516
George Harries <george.harries@gmail.com>
2023-03-13 08:48:45 +0000

    Adding dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use bevy::prelude::*;
+// use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+// Window constants
+pub const ASPECT_RATIO: f32 = 16.0/9.0;
+pub const WINDOW_HEIGHT: f32 =  720.0;
+pub const WINDOW_WIDTH: f32 = WINDOW_HEIGHT*ASPECT_RATIO;
+
+////////////////////////////////////////////////////////////////
+// Components
+////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////
+// App
+////////////////////////////////////////////////////////////////
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.8)))
+    .add_plugins(DefaultPlugins.set(
+        WindowPlugin { 
+            primary_window: Some(Window {
+                title: "Bevy Boids Implementation".to_string(),
+                resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }
+    ))
+    // .add_plugin(WorldInspectorPlugin)
+    .add_startup_system(spawn_camera)
+    .run();
 }
+
+////////////////////////////////////////////////////////////////
+// Systems
+////////////////////////////////////////////////////////////////
+fn spawn_camera(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+////////////////////////////////////////////////////////////////
+// Helper functions
+////////////////////////////////////////////////////////////////
